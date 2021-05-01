195d0da5be68985d8de2a4dffe357f01d4909cd3
ahanik <hannan.anik@northsouth.edu>
2021-05-02 00:43:02 +0600

    Homepage file
    Signed-off-by: ahanik <hannan.anik@northsouth.edu>

--- Homepage.java
+++ Homepage.java
@@ -1,11 +1,39 @@
 package tripzobd.com;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Homepage extends AppCompatActivity {
+
+    AutoCompleteTextView autoCompleteTextView1;
+    AutoCompleteTextView autoCompleteTextView2;
+    DatabaseReference databaseReference;
+    ValueEventListener listener;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> spinnerDataList;
+    EditText journeydate;
+    DatePickerDialog.OnDateSetListener setListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +44,63 @@ public class Homepage extends AppCompatActivity {
         getSupportActionBar().hide();
         //thats all
         setContentView(R.layout.activity_homepage);
+        autoCompleteTextView1 = (AutoCompleteTextView) findViewById(R.id.source);
+        autoCompleteTextView2 = (AutoCompleteTextView) findViewById(R.id.destination);
+
+        journeydate = findViewById(R.id.date);
+        Calendar calendar = Calendar.getInstance();
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Ticket counter");
+        spinnerDataList = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(Homepage.this,android.R.layout.simple_spinner_dropdown_item, spinnerDataList);
+        autoCompleteTextView1.setAdapter(adapter);
+        autoCompleteTextView2.setAdapter(adapter);
+        retriveData();
+
+
+       final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        journeydate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(Homepage.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        month = month+1;
+                        String date = day+"/"+month+"/"+year;
+                       journeydate.setText(date);
+                    }
+                },year,month,day);
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+                datePickerDialog.show();
+            }
+        });
+
+
+
+    }
+
+
+
+
+
+
+    public void retriveData(){
+        listener = databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot item:snapshot.getChildren()){
+                    spinnerDataList.add(item.getValue().toString());
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
