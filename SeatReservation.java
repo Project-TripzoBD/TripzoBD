4740255f916d1a306cece9e7b29eb6810fb6c6ea
shaon-das <shaon.das@northsouth.edu>
2021-05-03 15:35:18 +0600

    Seat Reservation java File
    Signed-off-by: shaon-das <shaon.das@northsouth.edu>

---
+++ SeatReservation.java
@@ -0,0 +1,14 @@
+package tripzobd.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeatReservation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seat_reservation);
+    }
+}
