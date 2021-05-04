b9537a6057f201b051f0a1778b5b44b79e4c8689
PinakiRanjan <pinaki.ranjan@northsouth.edu>
2021-05-04 22:45:01 +0600

    Main Activity file
    Signed-off-by: PinakiRanjan <pinaki.ranjan@northsouth.edu>

--- MainActivity.java
+++ MainActivity.java
@@ -1,65 +1,43 @@
-package com.example.pinakiranjandas;
+package com.example.adminpanel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.Window;
-import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button addbuscompanybtn,connectList;
 
-
-    private static int SPLASH_SCREEN = 5000;
-
-    //Variables
-    Animation topAnim, bottomAnim;
-    ImageView image;
-    TextView logo, slogan;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-
-
         setContentView(R.layout.activity_main);
+        addbuscompanybtn = (Button) findViewById(R.id.addbuscompany);
+        connectList = (Button) findViewById(R.id.list);
 
-        //Animations
-        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
-        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_amimation);
 
-        //Hooks
-        image = findViewById(R.id.imageView);
-        logo = findViewById(R.id.textView);
-        slogan = findViewById(R.id.textView5);
-
-        image.setAnimation(topAnim);
-        logo.setAnimation(bottomAnim);
-        slogan.setAnimation(topAnim);
-
-        new Handler().postDelayed(new Runnable() {
+        addbuscompanybtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this,Dashboard.class);
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, addbuscompany.class);
                 startActivity(intent);
-                finish();
-
-
             }
-        },SPLASH_SCREEN);
+        });
 
-
+        connectList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this,viewbuslist.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
+
+
 }
