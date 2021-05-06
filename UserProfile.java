353d943e97f79f4ed177d951752a16173344e197
shaon-das <shaon.das@northsouth.edu>
2021-05-06 20:00:06 +0600

    User Profile Java File Updated
    Signed-off-by: shaon-das <shaon.das@northsouth.edu>

--- UserProfile.java
+++ UserProfile.java
@@ -1,21 +1,45 @@
-package com.example.tripzobdcom;
-
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+package tripzobd.com;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.os.Bundle;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import androidx.cardview.widget.CardView;;
 
 public class UserProfile extends AppCompatActivity {
+
+    private Button buttonUpdate;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
+        firebaseAuth= FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        buttonUpdate = (Button) findViewById(R.id.button);
+
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
